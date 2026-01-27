62d9588204d8ba41668b57b149bd01f36a293898
Seif Ayman <167328383+S3fpl@users.noreply.github.com>
2026-01-27 02:10:05 +0200

    search: delete the 3 dots

--- app/(components)/Navbar/index.tsx
+++ app/(components)/Navbar/index.tsx
@@ -24,7 +24,7 @@ const Navbar = ({ toggleSidebar }: NavbarProps) => {
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
           <input
             type="search"
-            placeholder="Search..."
+            placeholder="Search"
             className="pl-10 pr-4 py-2 w-full sm:w-64 border border-gray-200 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
           />
         </div>
